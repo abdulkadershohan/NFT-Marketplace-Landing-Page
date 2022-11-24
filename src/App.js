36abccf7db862bccffe9bd7b0b7f6c7a6d68bad9
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-11-24 10:52:05 +0600

    font setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import './font.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit Trending NFTs <code>src/App.js</code> Elias is a web designer and front-end developer
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
