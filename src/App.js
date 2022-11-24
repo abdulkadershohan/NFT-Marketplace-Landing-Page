efd0923fb0bbfde5c94c1f0b0a81cbe8f8fbc7dd
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-11-24 12:02:10 +0600

    aa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import './App.css';
+import { Stack } from '@mui/system';
+import Header from './components/header/Header';
 import './font.css';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Trending NFTs <code>src/App.js</code> Elias is a web designer and front-end developer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack>
+      <Header />
+
+    </Stack>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import { Stack } from "@mui/system";
+import React from "react";
+import Navbar from "./navbar/Navbar";
+
+export default function Header() {
+    return (
+        <Stack>
+            <Navbar />
+        </Stack>
+    );
+}
